ff1b6158ab30ad5875fa53d0545601811c5a1ff8
= <=>
2021-10-25 11:12:51 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+// Use the fetch api to display some simple user data!
+
+url = 'https://reqres.in/api/users.json'
+
+
+// the function that will return the final string to be added to our HTML
+
+function stringBuilder(data) {
+    //do something
+}
+
+
+// call the fetch api with the correct input argument!
+fetch()
+    // convert the returned data into a json using the object's built-in methods
+    .then()
+    // pass the resulting json into our stringBuilder function and then display the information on our web page!
+    .then()
+    // console.log an error message in case something goes wrong
+    .catch()
