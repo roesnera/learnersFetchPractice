8532686e14915bbe2f94326336db3bf4d9f5f5d4
roesnera <adam.roesner@generaiton.org>
2022-03-21 16:37:29 -0400

    Got rid of DOM elements to simplify activity.

--- app.js
+++ app.js
@@ -1,23 +1,17 @@
-const button = document.getElementById('getResults')
-button.addEventListener('click', function() {
-// Use the fetch api to display some simple user data!
+/*
+    In this activity, you will be making a simple fetch api call to the url provided to request some data stored there.
+    In the first stage of this challenge, your task is to use the provided code to make the fetch request and then console log the data.
+    If your team has time, update our code using the async/await keywords!
+*/
 
-url = 'https://reqres.in/api/users'
+function makeCall() {
+  // Use the fetch api to display some simple user data to the console!
+  url = "https://reqres.in/api/users";  
 
+  
+  fetch() // call the fetch api with the correct input argument!
+    .then() // convert the returned data into a json using the object's built-in methods.
+    .then() // use the resulting json to display some of our user information in the console.
+    .catch(); // console.log an error message in case something goes wrong.
 
-// the function that will return the final string to be added to our HTML
-
-function stringBuilder(data) {
-    //do something
-}
-
-
-// call the fetch api with the correct input argument!
-fetch()
-    // convert the returned data into a json using the object's built-in methods
-    .then()
-    // pass the resulting json into our stringBuilder function and then display the information on our web page!
-    .then()
-    // console.log an error message in case something goes wrong
-    .catch()
-})
+};
